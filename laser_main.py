71e42c2e46b4c2d21ebd487c31d2d50fef9ec073
kino-n1851 <kino.n1851@gmai.com>
2021-10-06 16:36:17 +0900

    work

--- laser_main.py
+++ laser_main.py
@@ -5,17 +5,41 @@ from pprint import pprint as pp
 import socket
 import json
 
+class ColorConfig:
+    
+    def __init__(self):
+        self.h_min = 0
+        self.h_max = 179
+        self.s_min = 0
+        self.s_max = 255
+        self.v_min = 0
+        self.v_max = 255
+
+
 #udpサーバのアドレス
 serv_addr=('127.0.0.1',2323)
 cap = None
+config_red = ColorConfig()
+config_green = ColorConfig()
+
 def pass_(*args, **kwargs):
         pass
 
 def init():
     global cap
-    cap = cv2.VideoCapture(0)
+
+    cap = cv2.VideoCapture(2)
     imageWidth = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     imageHeight = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    
+
+def colorConfigure():
+    global cap
+    global config_red
+    global config_green
+    isSetRed = False
+    isSetGreen = False
+
     cv2.namedWindow("config_red")
     cv2.createTrackbar("H_min", "config_red", 0, 179, pass_)       # Hueの最大値は179
     cv2.createTrackbar("H_max", "config_red", 128, 179, pass_)
@@ -24,6 +48,27 @@ def init():
     cv2.createTrackbar("V_min", "config_red", 128, 255, pass_)
     cv2.createTrackbar("V_max", "config_red", 255, 255, pass_)
 
+    while not isSetRed:
+        ret,frame = cap.read()
+        if  (frame is None) or (frame.size == 0):
+            return -1
+        config_red.h_min = cv2.getTrackbarPos("H_min", "config_red")
+        config_red.h_max = cv2.getTrackbarPos("H_max", "config_red")
+        config_red.s_min = cv2.getTrackbarPos("S_min", "config_red")
+        config_red.s_max = cv2.getTrackbarPos("S_max", "config_red")
+        config_red.v_min = cv2.getTrackbarPos("V_min", "config_red")
+        config_red.v_max = cv2.getTrackbarPos("V_max", "config_red")
+        hsv_img =  cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+        mask_img = cv2.inRange(hsv_img, (h_min_red, s_min_red, v_min_red, v_max_red), (h_max_red, s_max_red, v_max_red))
+        result_img = cv2.bitwise_and(hsv_img, hsv_img, mask=mask_img)
+        result_img = cv2.cvtColor(result_img, cv2.COLOR_HSV2RGB)
+        result_img = cv2.GaussianBlur(result_img,(3,3),1)
+        cv2.imshow("config_red", result_img)
+        key = cv2.waitKey(1)
+        if key == 27:                   # k が27(ESC)だったらwhileループを脱出，プログラム終了
+            isSetRed = True
+            cv2.destroyAllWindows()
+    
     cv2.namedWindow("config_green")
     cv2.createTrackbar("H_min", "config_green", 0, 179, pass_)       # Hueの最大値は179
     cv2.createTrackbar("H_max", "config_green", 128, 179, pass_)
@@ -32,35 +77,33 @@ def init():
     cv2.createTrackbar("V_min", "config_green", 128, 255, pass_)
     cv2.createTrackbar("V_max", "config_green", 255, 255, pass_)
 
-def configure():
-    global cap
-    ret,frame = cap.read()
-    if  (frame is None) or (frame.size == 0):
-        return -1
-    h_min_red = cv2.getTrackbarPos("H_min", "config_red")
-    h_max_red = cv2.getTrackbarPos("H_max", "config_red")
-    s_min_red = cv2.getTrackbarPos("S_min", "config_red")
-    s_max_red = cv2.getTrackbarPos("S_max", "config_red")
-    v_min_red = cv2.getTrackbarPos("V_min", "config_red")
-    v_max_red = cv2.getTrackbarPos("V_max", "config_red")
-    hsv_img =  cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-    mask_img = cv2.inRange(hsv_img, (h_min_red, s_min_red, v_min_red, v_max_red), (h_max_red, s_max_red, v_max_red))
-    result_img = cv2.bitwise_and(hsv_img, hsv_img, mask=mask_img)
-    result_img = cv2.cvtColor(result_img, cv2.COLOR_HSV2RGB)
-    result_img = cv2.GaussianBlur(result_img,(3,3),1)
-    cv2.imshow("config_red", result_img)
-    key = cv2.waitKey(1)
-    if key == 27:                   # k が27(ESC)だったらwhileループを脱出，プログラム終了
-        return 1
-
+    while not isSetGreen:
+        ret,frame = cap.read()
+        if  (frame is None) or (frame.size == 0):
+            return -1
+        h_min_red = cv2.getTrackbarPos("H_min", "config_green")
+        h_max_red = cv2.getTrackbarPos("H_max", "config_green")
+        s_min_red = cv2.getTrackbarPos("S_min", "config_green")
+        s_max_red = cv2.getTrackbarPos("S_max", "config_green")
+        v_min_red = cv2.getTrackbarPos("V_min", "config_green")
+        v_max_red = cv2.getTrackbarPos("V_max", "config_green")
+        hsv_img =  cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+        mask_img = cv2.inRange(hsv_img, (h_min_red, s_min_red, v_min_red, v_max_red), (h_max_red, s_max_red, v_max_red))
+        result_img = cv2.bitwise_and(hsv_img, hsv_img, mask=mask_img)
+        result_img = cv2.cvtColor(result_img, cv2.COLOR_HSV2RGB)
+        result_img = cv2.GaussianBlur(result_img,(3,3),1)
+        cv2.imshow("config_green", result_img)
+        key = cv2.waitKey(1)
+        if key == 27:                   # k が27(ESC)だったらwhileループを脱出，プログラム終了
+            isSetGreen = True
+            cv2.destroyAllWindows()
+    
+    
 def main() -> None:
 
     init()
-    while True:
-        configure()
-
-    
-
+    colorConfigure()
+    print("end")
 
 if __name__ == "__main__":
     main()
